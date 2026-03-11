8f21a65bf1776123a2d868d151bf350dd1e24e2b
Juan Gutierrez <jugutierrez@esri.co>
2026-03-10 19:11:25 -0500

    fix: ID correcto de Luis Miguel

--- js/api.js
+++ js/api.js
@@ -20,7 +20,8 @@ async function spotifyFetch(endpoint) {
 // Obtener lista de álbumes de un artista conocido (ej: Taylor Swift)
 async function getAlbums() {
   // ID de Taylor Swift como ejemplo, puede cambiarse
-  const data = await spotifyFetch('/artists/2nszmSgqreOHnfuX9U1CDg/albums?limit=10&include_groups=album');
+  // CORRECTO
+  const data = await spotifyFetch('/artists/2nszmSgqreHSdJA3zWPyrW/albums?limit=10&include_groups=album');
   return data.items;
 }
 
